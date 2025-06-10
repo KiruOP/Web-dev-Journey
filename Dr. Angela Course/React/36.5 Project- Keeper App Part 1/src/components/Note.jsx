0652be2eafcca7cba7ded49d1ba4111093b999e7
KiruOP <kiranbiradar740@gmail.com>
2025-06-11 00:23:09 +0530

    Add initial project files for Keeper App with components, styles, and entry point

---
+++ Dr. Angela Course/React/36.5 Project- Keeper App Part 1/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>heading of note</h1>
+      <p>paragraph for note</p>
+    </div>
+  );
+}
+
+export default Note;
